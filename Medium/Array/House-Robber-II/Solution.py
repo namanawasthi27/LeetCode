09f16b7219c1d987e5ac70845a1aaf4d66fa723e
namanawasthi27 <namanawasthi2763@gmail.com>
2026-08-08 15:53:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (31.40%) - GetLeet

--- Medium/Array/House-Robber-II/Solution.py
+++ Medium/Array/House-Robber-II/Solution.py
@@ -4,10 +4,11 @@ class Solution:
         
         if n==1:
             return nums[0]
+        if n==2:
+            return max(nums)
         def helper(nums):
             n=len(nums)
-            if n==1:
-                return nums[0]
+            
             dp=[0]*n
             dp[0]=nums[0]
             dp[1]=max(nums[0],nums[1])
