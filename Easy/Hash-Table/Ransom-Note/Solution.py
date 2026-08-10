82e89f8f12c235db7034c39f2f67fea7b71fc1b4
namanawasthi27 <namanawasthi2763@gmail.com>
2026-08-10 16:08:45 +0530

    Time: 27 ms (34.00%) | Memory: 19.5 MB (94.01%) - GetLeet

---
+++ Easy/Hash-Table/Ransom-Note/Solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(ransomNote)>len(magazine):
+            return False
+        d={}
+        for i in magazine:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        for i in ransomNote:
+            if i not in d or d[i]==0:
+                return False
+            d[i]-=1
+        return True
