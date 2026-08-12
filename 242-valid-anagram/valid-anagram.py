32fed719e764d49dd12bbcce5056fc5b7975c365
namanawasthi27 <namanawasthi2763@gmail.com>
2026-08-13 03:19:34 +0530

    Time: 7 ms (91.65%) | Memory: 19.4 MB (46.28%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,9 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
+        if len(s)<len(t):
             return False
-        g=sorted(s)
-        f=sorted(t)
-        if g==f:
+        d={}
+        j={}
+        for i in range(0,len(s)):
+            if s[i] in d:
+                d[s[i]]+=1
+            else:
+                d[s[i]]=1
+        for i in range(0,len(t)):
+            if t[i] in j:
+                j[t[i]]+=1
+            else:
+                j[t[i]]=1   
+        if d==j:
             return True
-        return False
+        else:
+            return False
