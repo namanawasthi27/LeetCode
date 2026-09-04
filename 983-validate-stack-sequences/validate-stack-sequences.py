d434f27c0e193330e864bd5fccd3c0082e925622
namanawasthi27 <namanawasthi2763@gmail.com>
2026-09-04 15:41:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (53.85%) - LeetSync

--- 983-validate-stack-sequences/validate-stack-sequences.py
+++ 983-validate-stack-sequences/validate-stack-sequences.py
@@ -7,4 +7,4 @@ class Solution:
             while i<len(popped) and stack and popped[i]==stack[-1]:
                 stack.pop()
                 i+=1
-        return not stack
+        return not stack  #if condition not "False" else "True"
